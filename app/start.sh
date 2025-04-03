be5c13629b929f682b153b85153693ecb8957c27
Yanie <maanadelacruz@gbox.ncf.edu.ph>
2025-04-03 20:01:01 +0800

    Deployment Updates

--- app/start.sh
+++ app/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# This script is used to start the application using Gunicorn.
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src
 gunicorn -w 4 -b 0.0.0.0:$PORT app.main:app
