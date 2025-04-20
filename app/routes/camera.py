1f9642df63ac6a96682e5660467cb59385c856f8
Yanie <maanadelacruz@gbox.ncf.edu.ph>
2025-04-21 00:37:17 +0800

    Additional Route

--- app/routes/camera.py
+++ app/routes/camera.py
@@ -7,3 +7,8 @@ camera_bp = Blueprint('camera', __name__, url_prefix='/api/camera')
 @camera_bp.route('/upload', methods=['POST'], endpoint='upload_image')
 @handle_errors 
 def upload_image(): return camera.upload_image()
+
+@camera_bp.route('/process', methods=['GET'], endpoint='process_latest')
+@handle_errors
+@protected_route
+def process_latest(): return camera.process_latest()
